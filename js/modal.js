060c2949388b394f4ff868ff5d6c0d565898d8a2
michelD-dev <delaunaymichel@hotmail.fr>
2022-03-23 09:27:22 +0100

    Modale de refus âge non autorisé

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,5 @@
+
+
 function editNav() {
   var x = document.getElementById('myTopnav')
   if (x.className === 'topnav') {
@@ -6,7 +8,6 @@ function editNav() {
     x.className = 'topnav'
   }
 }
-//TODO faire un return (condition) dans les tests validation...
 
 // DOM Elements
 const modalbg = document.querySelector('.bground')
@@ -15,8 +16,10 @@ const modalCloseBtn = document.querySelector('.close')
 const formData = document.querySelectorAll('.formData')
 
 const confirmModalbg = document.querySelector('.bground__confirm')
+const refuseModalbg = document.querySelector('#not-of-legal-age')
 const confirmModalBtn = document.querySelector('.btn-submit')
 const confirmModalCloseBtn = document.querySelector('.btn-close')
+const refuseModalCloseBtn = document.querySelector('#refuse-btn-close')
 
 // launch modal form event
 modalBtn.forEach(btn => btn.addEventListener('click', launchModal))
@@ -35,11 +38,25 @@ function launchConfirmModal() {
   confirmModalbg.style.display = 'block'
 }
 
+//launch refuse modal
+function launchRefuseModal() {
+  refuseModalbg.style.display = 'block'
+}
+
 // close modal form event //FIXME
 modalCloseBtn.addEventListener('click', closeModal)
 
-// close confirmation modal event
+// close confirmation modal addEventListener
 confirmModalCloseBtn.addEventListener('click', closeConfirmModal)
+modalbg.addEventListener('keypress', e => {
+  if (e.key === 'enter') closeConfirmModal
+})
+
+// close refuse modal events
+refuseModalCloseBtn.addEventListener('click', closeRefuseModal)
+refuseModalbg.addEventListener('keypress', e => {
+  if (e.key === 'enter') closeRefuseModal  //FIXME
+})
 
 // close modal form
 function closeModal() {
@@ -51,14 +68,21 @@ function closeConfirmModal() {
   confirmModalbg.style.display = 'none'
 }
 
+// close refuse modal
+function closeRefuseModal() {
+  refuseModalbg.style.display = 'none'
+}
+
 // FONCTION MESSAGE D'ERREUR:
 const errorMessage = (id, message) => {
   const error = document.getElementById(`${id}-error`)
   error.textContent = message
-  // if (id !== 'location' || id !== 'CGU') document.getElementById(id).classList.add('error')
-  id !== 'location' &&
-    id !== 'CGU' &&
+  if (id !== 'location' && id !== 'CGU') {
     document.getElementById(id).classList.add('error')
+  }
+  // if (message === '') {
+  //   document.getElementById(id).classList.remove('error')
+  // }
 }
 
 // FORM VALIDATION FUNCTION
@@ -67,38 +91,44 @@ const validate = e => {
 
   // FIRSTNAME VALIDATION:
   const firstName = document.getElementById('first')
-  if (firstName.value.length < 2) {
-    //TODO ajouter validation regex (a/9/_/-/./é)
+  if (!/[a-z][a-z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœ-_.]{1,}/.test(first.value.toLowerCase())) {
     errorMessage('first', 'Vous devez indiquer un prénom valide.')
     firstName.focus()
     return
   } else {
     errorMessage('first', '')
+    first.value = first.value.toLowerCase()
   }
 
   // LASTNAME VALIDATION:
   const lastName = document.getElementById('last')
-  if (lastName.value.length < 2) {
+  if (!/[a-zA-Z][a-zA-Z0-9-_.]{1,}/.test(last.value.toLowerCase())) {
     errorMessage('last', 'Vous devez indiquer un nom valide.')
     lastName.focus()
     return
   } else {
     errorMessage('last', '')
+    last.value = last.value.toLowerCase()
   }
 
   // EMAIL VALIDATION:
   const email = document.getElementById('email')
-  if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+  if (
+    !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+      email.value.toLowerCase()
+    )
+  ) {
     errorMessage('email', 'Vous devez indiquer une adresse mail valide.')
     email.focus()
     return
   } else {
     errorMessage('email', '')
+    email.value = email.value.toLowerCase()
   }
 
   // BIRTHDATE VALIDATION:
   const birthDate = document.getElementById('birthdate')
-  if (!birthDate.value) {
+  if (!birthDate.value || new Date(birthDate.value).getTime() > Date.now()) {
     errorMessage('birthdate', 'Vous devez indiquer une date de naissance.')
     birthDate.focus()
     return
@@ -137,10 +167,19 @@ const validate = e => {
   }
 
   // VALIDATION:
-  document.querySelectorAll('input:not([type="button"])').forEach(input => {
-    input.classList.remove('error')
-    input.value = ''
-  })
-  launchConfirmModal()
+  let timeDiff = Math.abs(Date.now() - new Date(birthDate.value).getTime())
+  let diffyears = Math.ceil(timeDiff / (1000 * 3600 * 24) / 365)
+  if (diffyears < 18) {
+    launchRefuseModal()
+  } else {
+    launchConfirmModal('')
+  }
+
+  document
+    .querySelectorAll('input:not([type="button"]):not([type="submit"])')
+    .forEach(input => {
+      input.classList.remove('error')
+      input.value = ''
+    })
   closeModal()
 }
